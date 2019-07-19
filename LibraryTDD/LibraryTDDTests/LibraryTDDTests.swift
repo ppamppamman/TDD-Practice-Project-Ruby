66e9febd9f0dad43bb8a5aa056aae5a7dc5b2d43
devssun <chocochic85@gmail.com>
2019-07-19 23:10:50 +0900

    [TEST] Test2. 책 대출 체크 Blue Cycle

--- LibraryTDD/LibraryTDDTests/LibraryTDDTests.swift
+++ LibraryTDD/LibraryTDDTests/LibraryTDDTests.swift
@@ -12,22 +12,27 @@ struct Book {
     let title: String
     let author: String
     let publisher: String
+    let isLoan: Bool
 }
 
 class Library {
     private var books: [Book] = []
     
     init() {
-        books.append(Book(title: "테스트 주도 개발", author: "켄트 벡", publisher: "인사이트(insight)"))
-        books.append(Book(title: "여행의 이유", author: "김영하", publisher: "문학동네"))
-        books.append(Book(title: "설민석의 삼국지 ", author: "설민석", publisher: "세계사"))
-        books.append(Book(title: "아주 작은 습관의 힘", author: "제임스 클리어", publisher: "비즈니스북스"))
-        books.append(Book(title: "죽음 1", author: "베르나르 베르베르", publisher: "열린책들"))
-        books.append(Book(title: "Go Go 카카오프렌즈 9", author: "김미영", publisher: "아울북"))
+        books.append(Book(title: "테스트 주도 개발", author: "켄트 벡", publisher: "인사이트(insight)", isLoan: false))
+        books.append(Book(title: "여행의 이유", author: "김영하", publisher: "문학동네", isLoan: true))
+        books.append(Book(title: "설민석의 삼국지 ", author: "설민석", publisher: "세계사", isLoan: false))
+        books.append(Book(title: "아주 작은 습관의 힘", author: "제임스 클리어", publisher: "비즈니스북스", isLoan: true))
+        books.append(Book(title: "죽음 1", author: "베르나르 베르베르", publisher: "열린책들", isLoan: false))
+        books.append(Book(title: "Go Go 카카오프렌즈 9", author: "김미영", publisher: "아울북", isLoan: true))
     }
     
     func isExistBookHaveTitle(_ title: String) -> Bool {
         return books.contains { $0.title == title }
+    }
+    
+    func getBook(title: String) -> Book {
+        return books.filter { $0.title.contains(title) }.first!
     }
 }
 
@@ -50,7 +55,7 @@ class LibraryTDDTests: XCTestCase {
     
     func testIsOnLoan() {
         // 책 대출 여부 확인
-        let oneBook = library.getBook()
-        XCTAssertTrue(oneBook.isLoan())
+        let oneBook = library.getBook(title: "테스트 주도 개발")
+        XCTAssertEqual(false, oneBook.isLoan)
     }
 }
