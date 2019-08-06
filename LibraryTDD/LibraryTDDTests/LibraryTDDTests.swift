41d9f96436b5b3b15fe49ebc559c0b905a6beb1f
devssun <chocochic85@gmail.com>
2019-08-06 22:50:25 +0900

    [TEST] Test6. 책 자동 반납 Green Cycle

--- LibraryTDD/LibraryTDDTests/LibraryTDDTests.swift
+++ LibraryTDD/LibraryTDDTests/LibraryTDDTests.swift
@@ -14,6 +14,7 @@ struct Book {
     var publisher: String
     var isLoan: Bool
     var resvCount: Int
+    var returnDay: Date
     
     enum LoanError: Error {
         case loanError
@@ -60,12 +61,12 @@ class Library {
     private var books: [Book] = []
     
     init() {
-        books.append(Book(title: "테스트 주도 개발", author: "켄트 벡", publisher: "인사이트(insight)", isLoan: true, resvCount: 5))
-        books.append(Book(title: "여행의 이유", author: "김영하", publisher: "문학동네", isLoan: true, resvCount: 3))
-        books.append(Book(title: "설민석의 삼국지", author: "설민석", publisher: "세계사", isLoan: false, resvCount: 0))
-        books.append(Book(title: "아주 작은 습관의 힘", author: "제임스 클리어", publisher: "비즈니스북스", isLoan: true, resvCount: 4))
-        books.append(Book(title: "죽음 1", author: "베르나르 베르베르", publisher: "열린책들", isLoan: false, resvCount: 0))
-        books.append(Book(title: "Go Go 카카오프렌즈 9", author: "김미영", publisher: "아울북", isLoan: true, resvCount: 0))
+        books.append(Book(title: "테스트 주도 개발", author: "켄트 벡", publisher: "인사이트(insight)", isLoan: true, resvCount: 5, returnDay: Date()))
+        books.append(Book(title: "여행의 이유", author: "김영하", publisher: "문학동네", isLoan: true, resvCount: 3, returnDay: Date()))
+        books.append(Book(title: "설민석의 삼국지", author: "설민석", publisher: "세계사", isLoan: false, resvCount: 0, returnDay: Date()))
+        books.append(Book(title: "아주 작은 습관의 힘", author: "제임스 클리어", publisher: "비즈니스북스", isLoan: true, resvCount: 4, returnDay: Date()))
+        books.append(Book(title: "죽음 1", author: "베르나르 베르베르", publisher: "열린책들", isLoan: false, resvCount: 0, returnDay: Date()))
+        books.append(Book(title: "Go Go 카카오프렌즈 9", author: "김미영", publisher: "아울북", isLoan: true, resvCount: 0, returnDay: Date()))
     }
     
     func isExistBookHaveTitle(_ title: String) -> Bool {
@@ -133,9 +134,12 @@ class LibraryTDDTests: XCTestCase {
     func testAutoReturnToLoanBook() {
         // 자동 반납 테스트
         var willReturnBook = library.getBook(title: "테스트 주도 개발")
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let today = dateFormatter.string(from: Date())
         // 대출일로 5일째되면 자동 반납된다
         // 흐음... 일단 대출하면 대출일과 반납일 저장
         // 오늘이 반납일이면 반납
-        XCTAssertTrue(willReturnBook.returnDay == "2019-08-06")
+        XCTAssertTrue(dateFormatter.string(from: willReturnBook.returnDay) == today)
     }
 }
